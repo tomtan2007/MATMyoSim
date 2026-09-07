9a87f0aa7da5defa3b301c561530d75eb71b96a9
Tom Tan <tomtan2007@gmail.com>
2026-09-07 18:05:07 -0400

    test: expose guarded landmark baseline

--- Code/Fitting/mava_codex/detect_mava_trace_landmarks.m
+++ Code/Fitting/mava_codex/detect_mava_trace_landmarks.m
@@ -42,7 +42,8 @@ if isempty(rise_idx)
     error('detect_mava_trace_landmarks:noRise', ...
         'Could not detect a sustained pre-peak rise.');
 end
-baseline = mean(y(trough_idx:max(trough_idx, rise_idx-1)));
+baseline_range = trough_idx:max(trough_idx, rise_idx-1);
+baseline = mean(y(baseline_range));
 
 L = struct('peak_index', peak_idx, 'trough_index', trough_idx, ...
     'rise_index', rise_idx, 'peak_time', t(peak_idx), ...
